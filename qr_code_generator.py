b7336383dfb6feca357bd416e3741f520f372134
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2023-01-09 22:10:23 +0600

    a simple qr code generater added with some customizing

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,4 +1,28 @@
+# # A simple black and white QR code generator program
+# import qrcode
+#
+# img = qrcode.make("https://fahimmuntasir1.netlify.app/")
+# img.save("portfolio.png")
+
+# Simple QrCode generator with some changes
+
 import qrcode
 
-img = qrcode.make("https://fahimmuntasir1.netlify.app/")
-img.save("portfolio.png")
+# What is including in qrcode
+data = "https://youtube.com/"
+
+# Create a QR code
+
+qr = qrcode.QRCode(
+    version=1,
+    box_size=10,
+    border=3
+)
+# Add data into qr varriable
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(
+    fill_color = "red",
+    back_color = "white"
+)
+img.save("youtube.png")
